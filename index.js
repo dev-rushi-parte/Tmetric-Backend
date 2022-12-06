efd31f0950eebb8b0fdb08e139ef61ce563d3e59
rushi2784 <96285307+rushi2784@users.noreply.github.com>
2022-12-06 17:13:14 +0530

    change check

--- index.js
+++ index.js
@@ -2,21 +2,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
- 
+
 const morgan = require("morgan");
 const cors = require("cors")
-  
+
 // files imports
 // const User = require("./Model/User.model")
 const AuthRouter = require("./Routes/Auth.routes");
 const MemberRouter = require("./Routes/Members.routes.js")
 // const PostRouter = require("./Routes/Post.routes");
 const userAuthentication = require("./Controller/Auth.middleware");
-const taskRoutes=require("./Routes/Task.routes")
+const taskRoutes = require("./Routes/Task.routes")
 const { ClientRouter } = require("./Routes/Client.routes");
-const {timeController} = require("./Routes/time.routes")
+const { timeController } = require("./Routes/time.routes")
 
-const {ProjectRouter} = require("./Routes/Project.routes")
+const { ProjectRouter } = require("./Routes/Project.routes")
 
 
 
@@ -26,22 +26,31 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("common"));
 
+
+app.get("/", async (req, res) => {
+
+    // const { email, userId } = req.body
+    // const user = await User.find({ _id: userId })
+    res.status(201).send("home page" + " " + "HII home page")
+})
+
+
 app.use("/time", timeController)
 // Login Singup routes
 app.use("/user", AuthRouter)
 
-app.use("/" ,taskRoutes);
+app.use("/", taskRoutes);
 
 
 
 // login user authentication middleware
 app.use(userAuthentication)
 
-app.use("/clients" , ClientRouter)
-app.use("/project" , ProjectRouter)
+app.use("/clients", ClientRouter)
+app.use("/project", ProjectRouter)
 
 app.use("/member", MemberRouter);
-app.use("/task" ,taskRoutes);
+app.use("/task", taskRoutes);
 
 //  User Routes
 // app.use("/user", UserRouter);
